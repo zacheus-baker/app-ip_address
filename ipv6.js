f6a382fe37550064476390c72692ac4e4dfc1118
zacheus-baker <zacheus.baker@lumen.com>
2021-08-11 16:09:15 +0000

    Lab 4

--- ipv6.js
+++ ipv6.js
@@ -1,7 +1,4 @@
 function getIpv4MappedIpv6Address(ipv4) {
-
-    module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
-	
 	  // Initialize return argument
 	  let ipv6Address = null;
 	
@@ -43,3 +40,5 @@ function getIpv4MappedIpv6Address(ipv4) {
 	  }
 	  return ipv6Address;
 	}
+
+      module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
